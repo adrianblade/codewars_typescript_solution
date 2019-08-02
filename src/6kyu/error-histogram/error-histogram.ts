612b457e740d3cd5566be864c3a327269e41db8e
Adrián Catalán <adrian.catalan@travel-ims.com>
2019-08-02 21:20:20 +0200

    [error-histogram] initial commmit

--- src/6kyu/error-histogram/error-histogram.ts
+++ src/6kyu/error-histogram/error-histogram.ts
@@ -1,3 +1,11 @@
 export function hist(s: string): string {
-  return "u  2     **\rw  5     *****\rx  2     **\rz  31    *******************************";
+  const uCount= s.split("u").length - 1;
+  const wCount= s.split("w").length - 1;
+  const xCount= s.split("x").length - 1;
+  const zCount= s.split("z").length - 1;
+  const STAR = '*';
+  return "u  " + uCount + "     " + Array( uCount + 1 ).join(STAR)
+          + "\rw  " + wCount + "     " + Array( wCount + 1 ).join(STAR)
+          + "\rx  " + xCount + "     " + Array( xCount + 1 ).join(STAR)
+          + "\rz  " + zCount + "    " + Array( zCount + 1 ).join(STAR);
 }
